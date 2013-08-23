fe1355e7a16a239d96b12fb6b41031516c0727f2
Evgeny Cherkashin <eugene@irnok.net>
2013-08-23 22:44:44 +0900

    Stubs of the model classes added.

---
+++ FDModel/src/FDModel.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: eugeneai
+ * Date: 23.08.13
+ * Time: 22:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class FDModel {
+    public FDNode node;
+    public double euler(double dt) {
+        // Integrate graph in two steps
+    }
+}
---
+++ FDModel/src/FDNode.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: eugeneai
+ * Date: 23.08.13
+ * Time: 20:52
+ * To change this template use File | Settings | File Templates.
+ */
+public class FDNode {
+    public double val;
+    public double newVal;
+    public double oldVal;
+    public FDNode next;
+    public FDNode other;
+
+    FDNode(double aVal) {
+        this.val=aVal;
+        this.clean();
+    }
+    public void clean() {
+        this.newVal=0.0;
+        this.oldVal=0.0;
+    }
+}
