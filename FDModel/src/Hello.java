f8be106b266bf2ab1d2bbf8edce4f56bdb5d6444
Evgeny Cherkashin <eugene@irnok.net>
2013-08-23 20:52:07 +0900

    Hello class added.

---
+++ FDModel/src/Hello.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: eugeneai
+ * Date: 23.08.13
+ * Time: 20:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hello {
+    public static void main(String [] args) {
+        System.out.println("Hello World!");
+    }
+}
