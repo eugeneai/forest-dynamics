b3cb6c818d050ab45fcc1aa70f21749a1156450a
Evgeny Cherkashin <eugene@irnok.net>
2013-08-31 10:29:39 +0900

    Tried to construct some objects inside JavaScript environment.

--- FDModel/org/icc/fd/Scripting.java
+++ FDModel/org/icc/fd/Scripting.java
@@ -2,6 +2,9 @@ package org.icc.fd;
 
 import sun.org.mozilla.javascript.Context;
 import sun.org.mozilla.javascript.Scriptable;
+import sun.org.mozilla.javascript.ScriptableObject;
+
+import org.icc.fd.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,6 +20,20 @@ public class Scripting {
     Scripting () {
         this.context = Context.enter();
         this.scope = this.context.initStandardObjects();
+        //this.scope.put("a42", null, 42);
+        try {
+            ScriptableObject.defineClass(scope, JSMyCounter.class);
+        }
+        catch (Throwable e) {
+            System.err.println("JS init exception: "+e);
+        }
+        // Create one instance of Counter/JSMyCounter
+        Object[] arg = { new Integer(7) };
+        Scriptable myCounter = this.context.newObject(scope, "Counter", arg);
+        scope.put("myCounter", scope, myCounter);
+
+        //ScriptableObject.defineClass(scope, FDArc);
+        //ScriptableObject.defineClass(scope, FDNode);
         //TODO load our context objects as, e.g., a global variale FD, or fd;
     }
 
@@ -34,7 +51,18 @@ public class Scripting {
         return rc;
     }
 
-    public static void main(String [] args) {
+    public Object evaluateString(String cmd) {
+        return this.evaluateString(cmd, false);
+    }
+
+    public static void main (String [] args) {
         System.out.println("Javascript test.");
+        Scripting js=new Scripting();
+        Object rc = js.evaluateString("myCounter.count+1;");
+        System.out.print("Result:");
+        System.out.println(rc);
+        rc = js.evaluateString("myCounter.count+1;");
+        System.out.print("Result:");
+        System.out.println(rc);
     }
 }
