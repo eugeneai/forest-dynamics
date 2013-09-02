134a63d41d4c6b675e5feb7206d7403665f9d5fb
Evgeny Cherkashin <eugene@irnok.net>
2013-09-02 12:01:39 +0900

    Added scala test suite. Adjusted scala compiling environment.

---
+++ FDModel/org/icc/fd/HelloTest.scala
@@ -0,0 +1,14 @@
+package org.icc.fd
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eugeneai
+ * Date: 02.09.13
+ * Time: 8:58
+ * To change this template use File | Settings | File Templates.
+ */
+object HelloTest {
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+}
