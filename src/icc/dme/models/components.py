d17b537952daffdb330c9e7077132b67a6f03abd
Evgeny Cherkashin <eugeneai@irnok.net>
2013-09-12 17:23:55 +0900

    Connect simulation routines to the Project model.

--- src/icc/dme/models/components.py
+++ src/icc/dme/models/components.py
@@ -1,7 +1,23 @@
 from zope.interface import implements
 from interfaces import *
 
+from icc.dme.fd import *
+from icc.rake.views import get_user_config_option, set_user_config_option
+
 class Project(object):
+
     implements(IProject)
+
     def __init__(self):
-        self.test_attr="test"
+        self.setup_test_model()
+
+    def setup_test_model(self):
+        excel_file=get_user_config_option("source", type='string', keys='data_source')
+        if excel_file == None:
+            raise ValueError, "Forest data source is not supplied"
+        self.fm = ForestModel(excel_file, "1973",
+#		options={"NO_LOGGING":None, "NO_FIRES":None, "NO_INTERCHANGE":None}
+#		options={"NO_INTERCHANGE":None}
+#		options={"NO_INTERCHANGE":None,"NO_LOGGING":None, "NO_FIRES":None, "NO_GET":None}
+		options={"NO_INTERCHANGE":None,"NO_FIRES":None, "NO_GET":None}
+        )
