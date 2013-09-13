eed47f1c8eab7c0ab06f034ed1b8e26f600bda9f
Evgeny Cherkashin <eugeneai@irnok.net>
2013-09-13 21:51:34 +0900

    Makefile and test unit to debug pyxser.

---
+++ src/icc/test.py
@@ -0,0 +1,37 @@
+from icc.dme.fd.dm import *
+import pyxser
+
+class BB(object):
+    """
+    """
+
+    def __init__(self, ):
+        """
+        """
+
+
+
+class TestConnect(object):
+    """Test Class
+    """
+
+    def __init__(self):
+        """Init
+        """
+        A=DMItem(1)
+        B=DMItem(2)
+        C=DMConnect(A, B, 0.1)
+        self.y=(1,2,3)
+        self.b=BB()
+        self.c=C
+
+def main():
+    rc=None
+    a=TestConnect()
+    rc=pyxser.serialize(a, enc='utf-8')
+
+    print rc
+
+main()
+
+quit()
